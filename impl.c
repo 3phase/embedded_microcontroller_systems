47d9fab61cb743b9e686171321ccedb6741fb22a
3phase <pbozhinov@yahoo.com>
2016-01-19 19:00:47 +0200

    Updates according task

--- impl.c
+++ impl.c
@@ -1,32 +1,33 @@
 #include <stdio.h>
 
-int occurences[255] = {0};
+int occurences[15] = {0};
 
 void output_occurences_diagram() {
 	int i = 0;
 	int j = 0;
 	int k = 0;
 
-	for (i = 0; i < 255; i++) { 
+	for (i = 0; i <= 15; i++) { 
 		printf("%d:   ", i);
 		for (j = 0; j < occurences[i]; j++) {
 			printf("|");
 		}
 		printf("\n");
-		if (i%16 == 0) printf("\n\n");
 	}
 }
 
 void expand_occurences_table(int d) {
-	occurences[d]+=1;
+	int k = 0;
+	k = d / 16;
+	occurences[k]+=1;
 }
 
 int main() {
 	signed int d;		// integer with range - 0-255
 	while (1) {
-		printf("Submit an integer within 0-255: \n");
+		printf("Submit an integer within 0-256: \n");
 		scanf("%d",&d);
-		if (d < 0 || d >= 255) {
+		if (d < 0 || d >= 256) {
 			printf("Illegal integer was submitted.\n");
 			continue;
 		}
